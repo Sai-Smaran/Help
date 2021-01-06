c2e4eaaafb43de76029b5b49fd5422a29ca273ef
Sai-Smaran <74130069+Sai-Smaran@users.noreply.github.com>
2021-01-06 19:09:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var canvas, backgroundImage;
+
+var GameState = 0;
+var playerCount;
+var allPlayers;
+var database;
+
+var form, player, game;
+
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  if (playerCount === 4) {
+    game.update(1);
+  }
+  if (GameState === 1) {
+    clear();
+    game.play();
+  }
+}
